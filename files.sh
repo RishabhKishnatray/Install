31d2e08e07d46c6332c28baf1a04b1f673f869ab
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-26 12:51:06 +0530

    Update and rename files1 to files.sh

---
+++ files.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sudo apt update
+sudo apt-get install -y openjdk-8-jre-headless
+sudo update-java-alternatives --jre-headless -s java-1.8.0-openjdk-amd64
+sudo mkdir -p /etc/apt/keyrings
+sudo gpg --homedir /tmp --no-default-keyring --keyring /etc/apt/keyrings/scylladb.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys d0a112e067426ab2
+sudo wget -O /etc/apt/sources.list.d/scylla.list http://downloads.scylladb.com/deb/debian/scylla-5.2.list
+sudo apt-get update 
+sudo apt-get install -y scylla
