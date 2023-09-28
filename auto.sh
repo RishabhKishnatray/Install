5cefe61a17dc65b7cd3ba7a49af7b83c3a15f555
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-28 16:30:42 +0530

    Delete auto.sh

--- auto.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-git clone https://github.com/OT-MICROSERVICES/employee-api.git
-git clone https://github.com/RishabhKishnatray/Install.git
-sudo chmod 777 /home/ubuntu/Install/employeeconfig.sh
-sudo chmod 777 /home/ubuntu/Install/employeej.sh
-cd /home/ubuntu/Install
-./employeeconfig.sh
-./employeej.sh
