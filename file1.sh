ddd75a2495965e8ca05584936fce124afdc384a3
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-12-06 06:35:04 +0530

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -3,3 +3,10 @@ sudo apt update
 sudo apt install python3-pip -y
 sudo pip install boto3 botocore
 sudo pip3 install ansible==2.9.6
+
+
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.11
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
+sudo apt install python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm python3.11-tk python3.11-lib2to3
