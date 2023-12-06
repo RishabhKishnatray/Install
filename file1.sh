893c78623fdc24b66eadf10cce98140da87b43e3
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-12-06 11:58:52 +0530

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -12,8 +12,13 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 sudo apt install python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm python3.11-tk python3.11-lib2to3
 sudo yum install python311
 
-boto
+boto rhel
 
 sudo pip3 install boto3 botocore
 sudo ln -s /usr/bin/pip3 /usr/bin/pip
 sudo ln -s /usr/bin/pip3 /usr/bin/pip
+
+
+sudo amazon-linux-extras install epel
+sudo yum install ansible
+sudo yum install ansible
