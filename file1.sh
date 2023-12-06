9fcf99b17bc8e1420003e77b6f15e23ed64650d1
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-12-06 08:33:11 +0530

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -10,3 +10,4 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.11
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 sudo apt install python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm python3.11-tk python3.11-lib2to3
+sudo yum install python311
